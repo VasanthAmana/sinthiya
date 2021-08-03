0ef93138ff427829ab505ac8079d7b236494e5a1
root <root@LAPTOP-HQ5POD0V.localdomain>
2021-08-03 21:00:11 +0530

    rearraged column

--- sql-training/src/ex09.sql
+++ sql-training/src/ex09.sql
@@ -4,8 +4,8 @@
 -- updated_at(Should be in format YYYY-MM-DD), title
 -- Following tables are for the reference movies, movie_title_category_junctions, title_categories
 
-SELECT m.medium AS title_category, m.id AS brand_id, m.title AS brand_name, 
-CAST( m.created_at AS date), cast (m.updated_at as date)
+SELECT  m.id AS brand_id, m.title AS brand_name,   
+CAST( m.created_at AS date), cast (m.updated_at as date), m.medium AS title_category
 from movies m 
 INNER JOIN movie_title_category_junctions mj ON mj.id = m.id
 INNER JOIN title_categories tc ON tc.id = mj.id
