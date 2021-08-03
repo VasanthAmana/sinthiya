2cb70456f06bfb5134fbbcd98ae43b2728bc004f
root <root@LAPTOP-HQ5POD0V.localdomain>
2021-08-03 17:02:58 +0530

    Question Added

--- sql-training/src/ex09.sql
+++ sql-training/src/ex09.sql
@@ -1,3 +1,9 @@
+-- Select the movies that have the title_category THEATRICAL. 
+-- The following columns should be selected in a single query
+-- brand_id, brand_name, created_at(Should be in format YYYY-MM-DD), 
+-- updated_at(Should be in format YYYY-MM-DD), title
+-- Following tables are for the reference movies, movie_title_category_junctions, title_categories
+
 SELECT m.medium AS title_category, m.id AS brand_id, m.title AS brand_name, 
 CAST( m.created_at AS date), cast (m.updated_at as date)
 from movies m 
