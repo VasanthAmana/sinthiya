2975bc4a46d6ef443ae231ff01ab343a706f501c
root <root@LAPTOP-HQ5POD0V.localdomain>
2021-08-06 20:06:36 +0530

    exercises.md file updated and operator changed

--- sql-training/src/ex08.sql
+++ sql-training/src/ex08.sql
@@ -1,8 +1,10 @@
+-- Find the top 20 latest brands that have more than 1 instagram account and at least 1 pinterest user.
+
 SELECT m.title, m.id, ins.username,count(ins.username) insta_count,
 count (pin.data_source_uid) pin_uid_count
 FROM movies m
 LEFT JOIN instagram_accounts ins ON ins.movie_id = m.id
 LEFT JOIN brand_pinterest_users pin ON pin.movie_id = m.id
 GROUP BY m.id, ins.username
-HAVING COUNT(ins.username) > 1 AND COUNT(pin.data_source_uid) > 1
+HAVING COUNT(ins.username) > 1 AND COUNT(pin.data_source_uid) = 1
 ;
